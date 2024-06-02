6a54ce02211cb3cd7a725c255a6f2db8878c618b
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 21:43:49 +0500

    Create char32_t.cpp
    char32_t tipi foydalanilgan

---
+++ Basics/variables/char32_t/char32_t.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char32_t belgi = U'A'; // char32_t tipida o'zgaruvchi e'lon qilish
+    cout << "Belgi: " << char(belgi) << endl; // belgi o'zgaruvchisini chop etish
+    return 0;
+}

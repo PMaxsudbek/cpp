05b49a81e82ad55c02d6d690db535e226b7bc76e
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 22:00:28 +0500

    Create bitset.cpp
    bitset dan foydalanish

---
+++ Basics/variables/bitset/bitset.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+int main() {
+    bitset<8> bitlar("10101010"); // 8 bitli bitset o'zgaruvchisi e'lon qilish
+    cout << "Bitlar: " << bitlar << endl;// bitset o'zgaruvchisini chop etish
+    return 0;
+}

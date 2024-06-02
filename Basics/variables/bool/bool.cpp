44625ceaec9c1fa83d08d6922e8c315a4a214759
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 14:49:01 +0500

    Create bool.cpp
    Ushbu majburiyatda men C++ da bool  o'zgaruvchisidan foydalanish misolini qo'shdim. Kod false qiymatiga ega bool o'zgaruvchini ishga tushiradi, uni konsolga chop etadi, so'ngra operatsiyani aniq eslatmasdan o'zgaruvchini true ga yangilaydi va uni yana chop etadi. Bu C++ dasturida butun son oʻzgaruvchini qanday eʼlon qilish, ishga tushirish va yangilashni koʻrsatadi.

---
+++ Basics/variables/bool/bool.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    bool son = false;
+    cout << "Son = " << son << endl;  // Boshlang'ich qiymatni chiqaradi
+    son = true;
+    cout << "Son = " << son << endl;  // Yangilangan qiymatni chiqaradi
+    return 0;
+}

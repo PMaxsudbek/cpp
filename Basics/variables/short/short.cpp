4556a76c6a3e77c176585da77e4f8681bbeb4190
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 14:10:34 +0500

    Create short.cpp
    Ushbu majburiyatda men C++ da unsigned  o'zgaruvchisidan foydalanish misolini qo'shdim. Kod 5 qiymatiga ega butun o'zgaruvchini ishga tushiradi, uni konsolga chop etadi, so'ngra operatsiyani aniq eslatmasdan o'zgaruvchini 10 ga yangilaydi va uni yana chop etadi. Bu C++ dasturida butun son oʻzgaruvchini qanday eʼlon qilish, ishga tushirish va yangilashni koʻrsatadi.

---
+++ Basics/variables/short/short.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    short son = 5;
+    cout << "Son = " << son << endl;  // Boshlang'ich qiymatni chiqaradi
+    son = 10;
+    cout << "Son = " << son << endl;  // Yangilangan qiymatni chiqaradi
+    return 0;
+}

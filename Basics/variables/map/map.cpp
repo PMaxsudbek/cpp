0799ac5bef65ae68f4536a7f60ddb8a271e97448
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 22:42:13 +0500

    Update map.cpp

--- Basics/variables/map/map.cpp
+++ Basics/variables/map/map.cpp
@@ -2,7 +2,7 @@
 #include <map>
 using namespace std;
 int main() {
-    map<string, int> ismlar = {
+    map<string, int> jadval = {
     {"Pardayev",1},{"Maxsudbek",2},{"G'ulomjon o'g'li",3}
     };// std::map yaratish va boshlang'ich qiymatlarni kiritish
     cout << "So'z va tartibi:" << endl;// std::map elementlarini chop etish

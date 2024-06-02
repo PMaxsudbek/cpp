efeeb67191a1fa958eca55ac71a23bea1891b90a
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 14:55:12 +0500

    Create char.cpp
    Ushbu kodda char tipidagi o'zgaruvchi e'lon qilinyabdi va chop etilyabdi

---
+++ Basics/variables/char/char.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char belgi = 'A'; // char tipida o'zgaruvchi e'lon qilish
+    cout << "Belgi: " << belgi << endl; // belgi o'zgaruvchisini chop etish
+    return 0;
+}

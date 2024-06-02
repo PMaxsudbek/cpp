c045451a000bb9175c53fef064cbbb63d83c13f2
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 13:45:50 +0500

    Update int.cpp

--- Basics/variables/int/int.cpp
+++ Basics/variables/int/int.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     int son = 5;
     cout << "Son = " << son << endl;  // Boshlang'ich qiymatni chiqaradi
-    son = 10.5;
+    son = 10;
     cout << "Son = " << son << endl;  // Yangilangan qiymatni chiqaradi
     return 0;
 }

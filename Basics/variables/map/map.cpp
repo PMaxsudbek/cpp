85e2db005ba3e91a4b59227b61683c79322c65e9
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 22:31:30 +0500

    Create map.cpp
    map dan foydalanish

---
+++ Basics/variables/map/map.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main() {
+    map<string, int> ismlar = {
+    {"Pardayev",1},{"Maxsudbek",2},{"G'ulomjon o'g'li",3}
+    };// std::map yaratish va boshlang'ich qiymatlarni kiritish
+    cout << "So'z va tartibi:" << endl;// std::map elementlarini chop etish
+    for (const auto& pair : ismlar) {
+        cout << pair.first << " <-> " << pair.second << "- o'rin" << endl;
+    }
+}

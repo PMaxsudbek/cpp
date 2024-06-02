3fe558f4d763fa7cf70bd83cf09ca0e301182232
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 15:10:50 +0500

    Create string.cpp
    Ushbu kodda matn o'zgaruvchisi string tipida e'lon qilindi va chop etildi.

---
+++ Basics/variables/string/string.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string matn = "Hello world!"; // char tipida o'zgaruvchi e'lon qilish
+    cout << "Matn: " << matn << endl; // belgi o'zgaruvchisini chop etish
+    return 0;
+}

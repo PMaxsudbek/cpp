f01a70f5d81f426b7de4cf350453c141c392bf0c
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 14:04:20 +0500

    Create unsigned.cpp
    Ushbu majburiyatda men C++ da unsigned  o'zgaruvchisidan foydalanish misolini qo'shdim. Kod 5 qiymatiga ega butun o'zgaruvchini ishga tushiradi, uni konsolga chop etadi, so'ngra operatsiyani aniq eslatmasdan o'zgaruvchini 10 ga yangilaydi va uni yana chop etadi. Bu C++ dasturida butun son oʻzgaruvchini qanday eʼlon qilish, ishga tushirish va yangilashni koʻrsatadi.

---
+++ Basics/variables/unsigned/unsigned.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    unsigned son = 5;
+    cout << "Son = " << son << endl;
+    son = 10;
+    cout << "Son = " << son << endl;
+    return 0;
+}

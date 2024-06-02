a24b2963a875e094769eaac804717fff599b90ca
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 14:27:19 +0500

    Create int16_t.cpp
    Ushbu majburiyatda men C++ da unsigned  o'zgaruvchisidan foydalanish misolini qo'shdim. Kod 5 qiymatiga ega butun o'zgaruvchini ishga tushiradi, uni konsolga chop etadi, so'ngra operatsiyani aniq eslatmasdan o'zgaruvchini 10 ga yangilaydi va uni yana chop etadi. Bu C++ dasturida butun son oʻzgaruvchini qanday eʼlon qilish, ishga tushirish va yangilashni koʻrsatadi.

---
+++ Basics/variables/int16_t/int16_t.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+int main() {
+    int16_t son = 5;
+    cout << "Son = " << son << endl;  // Boshlang'ich qiymatni chiqaradi
+    son = 10;
+    cout << "Son = " << son << endl;  // Yangilangan qiymatni chiqaradi
+    return 0;
+}

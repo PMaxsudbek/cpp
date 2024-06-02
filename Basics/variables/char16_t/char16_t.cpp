7e8d252474e89bbf9867128092fc7d3fdae78e94
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-02 21:35:45 +0500

    Create char16_t.cpp
    char16_t tipidan foydalanilyabdi

---
+++ Basics/variables/char16_t/char16_t.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char16_t belgi = u'A'; // char16_t tipida o'zgaruvchi e'lon qilish
+    cout << "Belgi: " << char(belgi) << endl; // belgi o'zgaruvchisini chop etish
+    return 0;
+}

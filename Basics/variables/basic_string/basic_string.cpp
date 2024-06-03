921a944e90ad97f8a82c0a72563dd650b0584672
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-03 23:52:33 +0500

    Create basic_string.cpp
    basic_string dan foydalanish

---
+++ Basics/variables/basic_string/basic_string.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    // std::basic_string<char> ni std::string sifatida ishlatish
+    basic_string<char> a = "Salom, dunyo!";
+    cout << a << endl;
+    return 0;
+}

d78e334a3082fb6c8f41ff4ddc8bc2cc56492a69
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:17:21 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin34/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin34/main.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    float X, A, Y, B;
+    cout << "Shokoladning massasini kiriting ";cin >> X;
+    cout << "Shokoladning so'mmasini kiriting ";cin >> A;
+    cout << "Konfetning massasini kiriting ";cin >> Y;
+    cout << "Konfetning so'mmasini kiriting ";cin >> B;
+    cout << "1kg shokolad 1kg konfetdan ==>" << A / X - B / Y << "<== ga qimmat";
+}

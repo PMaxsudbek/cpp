52f165688839e136f2a596f51a6cd9e5c9168387
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:15:35 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin25/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin25/main.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float y, x;
+    cout << "x = ";cin >> x;
+    y = 3 * pow(x, 6) - 6 * x * x - 7;
+    cout << y;
+}

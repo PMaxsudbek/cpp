1e2af34d645d5b2c651b3de35969a64754cc5cad
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:19:18 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin9/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin9/main.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float a, b, S;
+    cout << "a = ";cin >> a;
+    cout << "b = ";cin >> b;
+    S = sqrt(a * b);
+    cout << char(251) << "(a * b) = " << S;
+}

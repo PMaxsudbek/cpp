b09ebcb313ffab98b15e49a71d10b9046a4321ac
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:15:56 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin27/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin27/main.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float A, A2, A4, A8;
+    cout << "A = ";cin >> A;
+    A2 = pow(A, 2);
+    A4 = pow(A, 4);
+    A8 = pow(A, 8);
+    cout << "A^2 = " << A2 << endl;
+    cout << "A^4 = " << A4 <<endl;
+    cout << "A^8 = " << A8;
+}

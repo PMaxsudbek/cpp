96c0f087d12527eb4a2d4d9a5384ee10263e9d23
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:13:04 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin13/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin13/main.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float R1, R2, S1, S2, S, pi = 3.14;
+    cout << "R1 = ";cin >> R1;
+    cout << "R2 = ";cin >> R2;
+    S1 = pi * R1 * R1;
+    S2 = pi * R2 * R2;
+    S = S1 - S2;
+    cout << "S = " << S;
+}

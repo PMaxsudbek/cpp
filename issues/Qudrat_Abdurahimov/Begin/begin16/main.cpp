9ad0ded9e6d88ebf980c68ffbf4e8f746bf84960
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:13:49 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin16/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin16/main.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float x1, x2;
+    cout << "x1 = ";cin >> x1;
+    cout << "x2 = ";cin >> x2;
+    cout << "|x1 - x2| = " << fabs(x1 - x2);
+}

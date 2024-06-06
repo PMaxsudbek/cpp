46abe01c41d8cb1de1c24eaaf9a6cb41faf7066d
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:17:39 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin36/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin36/main.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    float V1, V2, S, T;
+    cout << "1-avtomobilning tezligini kiriting ==>";cin >> V1;
+    cout << "2-avtomobilning tezligini kiriting ==>";cin >> V2;
+    cout << "Ular orasidagi masofani kiriting ==>";cin >> S;
+    cout << "Qancha vaqtdan kiyingi ular orasidagi masofa kerak ==>";cin >> T;
+    cout << T << " vaqtdan keyin ular orasidagi masofa ==>" << S + T * (V1 + V2);
+}

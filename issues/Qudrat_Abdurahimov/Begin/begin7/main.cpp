b2d60fe0f9ef5e52b92529d985954796c840a2cc
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:18:59 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin7/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin7/main.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    float R, L, S, pi = 3.14;
+    cout << "R = ";cin >> R;
+    L = 2 * pi * R;
+    S = pi * R * R;
+    cout << "L = " << L << endl << "S = " << S;
+}

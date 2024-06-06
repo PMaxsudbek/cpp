f6950c196a38c245e778c56a54c25e224458a17d
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:17:11 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin33/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin33/main.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    float X, A, y;
+    cout << "X = ";cin >> X;
+    cout << "A = ";cin >> A;
+    cout << "1kg va necha kilogramm konfetni narxi kerak ==>";cin >> y;
+    cout << "1kg konfetning so'mmasi ==>" << A / X << endl;
+    cout << y << "kg konfetning so'mmasi ==>" << A / X * y;
+}

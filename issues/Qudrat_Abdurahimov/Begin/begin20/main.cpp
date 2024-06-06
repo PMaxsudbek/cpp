3de281c3b1ecc93a0178ea10398261a11f8ed3b8
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:14:44 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin20/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin20/main.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float x1, x2, y1, y2, X;
+    cout << "Birinchi nuqta kordinatalarini kiriting" << endl;cin >> x1 >> y1;
+    cout << "Ikkinchi nuqta kordinatalarini kiriting" << endl;cin >> x2 >> y2;
+    X = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    cout << X;
+}

85b8449c4a6f9e25727d7e5b7d8b2ec283572367
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:18:08 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin39/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin39/main.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float A, B, C, x1, x2, D;
+    cout << "A = ";cin >> A;
+    cout << "B = ";cin >> B;
+    cout << "C = ";cin >> C;
+    D = B * B - 4 * A * C;
+    x1 = (-B + sqrt(D)) / (2 * A);
+    x2 = (-B - sqrt(D)) / (2 * A);
+    cout << "x1 = " << x1 << endl << "x2 = " << x2;
+}

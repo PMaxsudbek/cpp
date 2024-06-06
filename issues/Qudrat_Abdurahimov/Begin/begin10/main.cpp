40d9cf9cfd9a94184f0c415ece44646cc2c1f62e
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:12:25 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin10/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin10/main.cpp
@@ -1,1 +1,17 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float a, b, P, S, a2, b2;
+    cout << "a = ";cin >> a;
+    cout << "b = ";cin >> b;
+    P = a + b;
+    S = a * b;
+    a2 = a * a;
+    b2 = b * b;
+    cout << "a + b = " << P << endl;
+    cout << "a * b = " << S << endl;
+    cout << "a * a = " << a2 << endl;
+    cout << "b * b = " << b2;
+}

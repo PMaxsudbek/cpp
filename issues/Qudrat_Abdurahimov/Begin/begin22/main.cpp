ed5336b6fe78d316992fb4fc67b429beffc9422b
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:15:04 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin22/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin22/main.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float a, b, c;
+    cout << "a = ";cin >> a;
+    cout << "b = ";cin >> b;
+    c = a;
+    a = b;
+    b = c;
+    cout << "a = " << a << endl << "b = " << b;
+}

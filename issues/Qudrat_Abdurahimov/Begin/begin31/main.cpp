a17ca8810f9bfecca6ec13c8abaedd5bb5c882c2
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:16:49 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin31/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin31/main.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float Tf, Tc;
+    cout << "Tf = ";cin >> Tf;
+    Tc = (Tf - 32) * 5 / 9;
+    cout << "Tc = " << Tc;
+}

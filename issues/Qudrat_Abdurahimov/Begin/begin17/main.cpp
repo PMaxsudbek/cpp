3f95e73856d19ca1bdea9810e8ae27aa97cd110c
Pardayev Maxsudbek <125341724+PMaxsudbek@users.noreply.github.com>
2024-06-06 15:14:02 +0500

    Update main.cpp

--- issues/Qudrat_Abdurahimov/Begin/begin17/main.cpp
+++ issues/Qudrat_Abdurahimov/Begin/begin17/main.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    float A, B, C, AC, BC;
+    cout << "A = ";cin >> A;
+    cout << "B = ";cin >> B;
+    cout << "C = ";cin >> C;
+    AC = fabs(A - C);
+    BC = fabs(B - C);
+    cout << "AC = " << AC << endl << "BC = " << BC << endl << "AC + BC = " << AC + BC;
+}
